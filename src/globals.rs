ba78897160cf9eb9f285fa2041bd4eee7e878da1
ghaerdi <ghaerdi@outlook.com>
2021-10-18 14:01:05 -0300

    fix: remove dots on static file extensions

--- src/globals.rs
+++ src/globals.rs
@@ -13,8 +13,8 @@ pub static TEXT_EXT: [&str; 8] =
 pub static IMAGE_EXT: [&str; 9] = [
   "jpg", "png", "jpeg", "gif", "tiff", "psd", "bmp", "ico", "svg",
 ];
-pub static AUDIO_EXT: [&str; 4] = [".mp3", ".wma", ".wav", ".flac"];
+pub static AUDIO_EXT: [&str; 4] = ["mp3", "wma", "wav", "flac"];
 pub static VIDEO_EXT: [&str; 6] = ["mov", "mp4", "avi", "mkv", "flv", "wmv"];
 pub static COMPRESSED_EXT: [&str; 6] =
-  ["zip", "rar", "rar5", "7z", "ace", ".gz"];
+  ["zip", "rar", "rar5", "7z", "ace", "gz"];
 pub static EXECUTABLE_EXT: [&str; 4] = ["exe", "msi", "deb", "rpm"];
