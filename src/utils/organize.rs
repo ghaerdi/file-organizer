c1515e37ed3bfa7a1bce94100c3c78985817927a
ghaerdi <ghaerdi@outlook.com>
2021-10-19 14:03:23 -0400

    test: organize files

--- src/utils/organize.rs
+++ src/utils/organize.rs
@@ -32,3 +32,42 @@ pub fn organize(dir: &String) {
     }
   }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::utils;
+    use std::fs::File;
+    use std::fs;
+    use std::path::Path;
+
+    const DIR: &str = "./.move_file";
+
+    #[test]
+    fn organize_files() {
+        fs::create_dir(DIR).expect("unable to create dir");
+        utils::create_dirs(&DIR.to_string());
+
+        let files = vec!["foo.txt","foo.png","foo.mp3","foo.mp4","foo.zip","foo.exe"];
+
+        create_files(&files);
+        organize(&DIR.to_string());
+        
+        let expect = vec!["Text/foo.txt","Image/foo.png","Audio/foo.mp3","Video/foo.mp4","Compressed/foo.zip","Executable/foo.exe"];
+        assert_all(expect);
+    }
+
+    fn create_files(files: &Vec<&str>) {
+        for f in files {
+            let path = format!("{}/{}", DIR, f);
+            File::create(path).expect("unable to create file");
+        }
+    }
+
+    fn assert_all(files: Vec<&str>) {
+        for f in files {
+            let path = format!("{}/{}", DIR, f);
+            assert!(Path::new(&path).exists());
+        }
+    }
+}
